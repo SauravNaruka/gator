87352fd1d8199463f107d41384d810237c9e1c5d
SauravNaruka <saurav.singh.n@gmail.com>
2024-12-28 14:16:24 +0530

    Add config read write

---
+++ main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/SauravNaruka/gator/internal/config"
+)
+
+func main() {
+	cfg, err := config.Read()
+	if err != nil {
+		log.Fatalf("error reading config: %v", err)
+	}
+	fmt.Printf("Read config: %+v\n", cfg)
+
+	err = cfg.SetUser("snaruka")
+	if err != nil {
+		log.Fatalf("error writing config: %v", err)
+	}
+
+	cfg, err = config.Read()
+	if err != nil {
+		log.Fatalf("error reading config: %v", err)
+	}
+	fmt.Printf("Read config again: %+v\n", cfg)
+}
