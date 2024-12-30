eaeabbca7cc76144409ef54d283681ed2a65f12c
SauravNaruka <saurav.singh.n@gmail.com>
2024-12-30 19:53:30 +0530

    Add feed fetch

--- handler_agg.go
+++ handler_agg.go
@@ -3,14 +3,61 @@ package main
 import (
 	"context"
 	"fmt"
+	"log"
+	"time"
+
+	"github.com/SauravNaruka/gator/internal/database"
+	"github.com/SauravNaruka/gator/internal/gatorapi"
 )
 
 func handlerAgg(s *state, cmd command) error {
-	feed, err := s.gatorapiClient.FetchFeed(context.Background(), "https://www.wagslane.dev/index.xml")
-	if err != nil {
-		return fmt.Errorf("couldn't fetch feed: %w", err)
+	if len(cmd.Args) != 1 {
+		return fmt.Errorf("usage: %s <time_between_reqs>", cmd.Name)
 	}
 
-	fmt.Printf("Feed:%+v\n", feed)
-	return nil
+	timeBetweenRequests, err := time.ParseDuration(cmd.Args[0])
+	if err != nil {
+		return fmt.Errorf("invalid duration: %w", err)
+	}
+
+	fmt.Printf("Collecting feeds every %s", timeBetweenRequests)
+
+	ticker := time.NewTicker(timeBetweenRequests)
+	for ; ; <-ticker.C {
+		scrapeFeeds(s)
+	}
+}
+
+func scrapeFeeds(s *state) {
+	feed, err := s.db.GetNextFeedToFetch(context.Background())
+	if err != nil {
+		log.Println("Couldn't get next feeds to fetch", err)
+		return
+	}
+	log.Println("Found a feed to fetch!")
+	scrapeFeed(s, feed)
+}
+
+func scrapeFeed(s *state, feed database.Feed) {
+	_, err := s.db.MarkFeedFetched(context.Background(), feed.ID)
+	if err != nil {
+		log.Printf("Couldn't mark feed %s fetched: %v", feed.Name, err)
+		return
+	}
+
+	feedData, err := s.gatorapiClient.FetchFeed(context.Background(), feed.Url)
+	if err != nil {
+		log.Printf("Couldn't collect feed %s: %v", feed.Name, err)
+		return
+	}
+
+	printRSSFeed(feedData)
+}
+
+func printRSSFeed(feedData *gatorapi.RSSFeed) {
+	log.Printf("Feed %s collected, %v posts found", feedData.Channel.Title, len(feedData.Channel.Item))
+
+	for _, item := range feedData.Channel.Item {
+		fmt.Printf("Found post: %s\n", item.Title)
+	}
 }
